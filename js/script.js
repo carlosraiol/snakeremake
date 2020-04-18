c222319e56d878353ed202b74b2dac0e38e88b8f
Carlos Raiol <caremanuel@gmail.com>
2020-04-17 21:46:25 -0300

    Após Game Over, reinicia o jogo

--- js/script.js
+++ js/script.js
@@ -50,6 +50,7 @@ function iniciarJogo() {
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
             clearInterval(jogo);
             alert('Game Over! :(');
+            window.location.reload();
         }
     }
     criarBG();
